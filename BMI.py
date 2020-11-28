fa1d51988bcfadeaee9eaf9ecccf9078972550e6
str1ngs <martinmeerja@gmail.com>
2020-11-28 13:09:15 +0200

    added explanations

--- BMI.py
+++ BMI.py
@@ -1,15 +1,6 @@
 # A simple BMI calculator in imperial and metric system v.01
 # @str1ngs
 
-'''Less than 15 is Very severely underweight
-#Between 15 and 16 Severely underweight
-#Between 16 and 18.5 Underweight
-#Between 18.5 and 25 Normal (healthy weight)
-#Between 25 and 30 Overweight
-#Between 30 and 35 Moderately obese
-#Between 35 and 40 Severely obese
-#Over 40 Very severely obese'''
-
 measurement_system = str(input('Use Imperial or Metric measurement system: '))
 
 if measurement_system == 'Imperial':
@@ -17,8 +8,22 @@ if measurement_system == 'Imperial':
     height_in = float(input('Enter height in in: '))
 
     def bmi_imperial(weight_lbs, height_in):
-        bmi_imperial_value = 703 * weight_lbs / (height_in ** 2)
-        print('Your BMI is: ', int(bmi_imperial_value))
+        bmi_imperial_value = int(703 * weight_lbs / (height_in ** 2))
+        print('Your BMI is: {0}:' .format(bmi_imperial_value), end='')
+        if 15 > bmi_imperial_value:
+            print(', which is severely underweight.')
+        elif 15 < bmi_imperial_value < 18.5:
+            print(', which is underweight.')
+        elif 18.5 < bmi_imperial_value < 25:
+            print(', which is a healthy weight.')
+        elif 25 < bmi_imperial_value < 30:
+            print(', which is overweight.')
+        elif 30 < bmi_imperial_value < 35:
+            print(', which is moderately obese.')
+        elif 35 < bmi_imperial_value < 40:
+            print(', which is severely obese.')
+        elif 40 <= bmi_imperial_value:
+            print(', which is very severely obese.')
     bmi_imperial(weight_lbs, height_in)
 
 elif measurement_system == 'Metric':
@@ -26,9 +31,23 @@ elif measurement_system == 'Metric':
     height_m = float(input('Enter height in m: '))
 
     def bmi_metric(weight_kg, height_m):
-        bmi_metric_value = weight_kg / (height_m ** 2)
-        print('Your BMI is: ', int(bmi_metric_value))
+        bmi_metric_value = int(weight_kg / (height_m ** 2))
+        print('Your BMI is: {0} ' .format(bmi_metric_value), end='')
+        if 15 > bmi_metric_value:
+            print(', which is severely underweight.')
+        elif 15 < bmi_metric_value < 18.5:
+            print(', which is underweight.')
+        elif 18.5 < bmi_metric_value < 25:
+            print(', which is a healthy weight.')
+        elif 25 < bmi_metric_value < 30:
+            print(', which is overweight.')
+        elif 30 < bmi_metric_value < 35:
+            print(', which is moderately obese.')
+        elif 35 < bmi_metric_value < 40:
+            print(', which is severely obese.')
+        elif 40 <= bmi_metric_value:
+            print(', which is very severely obese.')
     bmi_metric(weight_kg, height_m)
 
 else:
-    print('You need to type either Imperial or Metric')
+    print('You need to type either Imperial or Metric!')
